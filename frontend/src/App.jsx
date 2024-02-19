296814ab6efd9b3d7a598e707752b04803abbb2e
Seroshtan_KV <seroshtan_kv@rtsoft.ru>
2024-02-19 16:42:16 +0300

    CRUD with many

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,30 @@ import styles from "./styles.module.css";
 
 const URL = 'http://127.0.0.1:5000';
 
+function generateRandomWord(length) {
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  let word = '';
+
+  for (let i = 0; i < length; i++) {
+      const randomIndex = Math.floor(Math.random() * alphabet.length);
+      word += alphabet[randomIndex];
+  }
+
+  return word;
+}
+
 function App() {
   const [item, setItem] = useState('');
+  const [secItem, setSecItem] = useState('');
   const [newItem, setNewItem] = useState('');
   const [id, setId] = useState('');
+  const [secId, setSecId] = useState('');
   const [items, setItems] = useState([]);
+
+  const handleGenerate = () => {
+    setItem(generateRandomWord(8))
+    setSecItem(generateRandomWord(8))
+  }
 
   // Функция для загрузки всех элементов
   const fetchItems = async () => {
@@ -35,6 +54,15 @@ function App() {
     }
   };
 
+  const addItems = async () => {
+    try { 
+      await axios.post(`${URL}/add_many`, { items: [{ name: item }, { name: secItem }] });
+      fetchItems(); // Перезагрузите элементы после добавления
+    } catch (error) {
+      console.error("Ошибка при добавлении данных:", error);
+    }
+  };
+
   // Функция для удаления элемента
   const deleteItem = async (id) => {
     try {
@@ -44,8 +72,17 @@ function App() {
       console.error("Ошибка при удалении данных:", error);
     }
   };
-  
 
+   // Функция для удаления элементов
+  const deleteItems = async () => {
+    try {
+      await axios.post(`${URL}/delete_many`, { ids: [id, secId] });
+      fetchItems(); // Перезагрузите элементы после удаления
+    } catch (error) {
+      console.error("Ошибка при удалении данных:", error);
+    }
+  };
+  
   // Функция для обновления элемента
   const updateItem = async () => {
     try {
@@ -73,7 +110,28 @@ function App() {
           placeholder="Введите название элемента"
           className={styles.input}
         />
-        <button onClick={addItem} className={styles.btn_main}>Добавить</button>
+        <button onClick={handleGenerate} className={styles.btn_main}>Сгенерировать слово</button>
+        <button onClick={addItem} className={styles.btn_second}>Добавить</button>
+      </div>
+
+      <div className={styles.header_medium}>Add many</div>
+      <div className={`${styles.flex} ${styles.flex_gap_medium}`}>
+        <input
+          type="text"
+          value={item}
+          onChange={(e) => setItem(e.target.value)}
+          placeholder="Введите название элемента"
+          className={styles.input}
+        />
+        <input
+          type="text"
+          value={secItem}
+          onChange={(e) => setSecItem(e.target.value)}
+          placeholder="Введите название элемента"
+          className={styles.input}
+        />
+        <button onClick={handleGenerate} className={styles.btn_main}>Сгенерировать слово</button>
+        <button onClick={addItems} className={styles.btn_second}>Добавить</button>
       </div>
 
       <div className={styles.header_medium}>Update</div>
@@ -92,12 +150,31 @@ function App() {
           placeholder="Новое название элемента"
           className={styles.input}
         />
-        <button onClick={updateItem} className={styles.btn_main}>Добавить</button>
+        <button onClick={updateItem} className={styles.btn_second}>Изменить</button>
+      </div>
+
+      <div className={styles.header_medium}>Delete many</div>
+      <div className={`${styles.flex} ${styles.flex_gap_medium}`}>
+        <input
+            type="text"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+            placeholder="Введите id элемента"
+            className={styles.input}
+        />
+        <input
+            type="text"
+            value={secId}
+            onChange={(e) => setSecId(e.target.value)}
+            placeholder="Введите id элемента"
+            className={styles.input}
+        />
+        <button onClick={deleteItems} className={styles.btn_danger}>Удалить</button>
       </div>
 
       <div className={styles.header_medium}>Get / Delete</div>
       <div>
-        <button onClick={fetchItems} className={styles.btn_second}>Загрузить элементы</button>
+        <button onClick={fetchItems} className={styles.btn_main}>Загрузить элементы</button>
         <ul className={`${styles.flex_column} ${styles.list}`}>
           {items.map((item, index) => (
             <li key={index} className={`${styles.flex} ${styles.flex_gap_medium}`}>
